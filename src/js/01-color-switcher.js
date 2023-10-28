b1856445a6a837782aaed203b6dadd7a977e3ad9
imlaamer <lolitayenik@ukr.net>
2023-10-28 22:22:51 +0300

    first task change color

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,2 +1,24 @@
+const bodyEl = document.querySelector('body');
+const btnStartEl = document.querySelector('button[data-start]');
+const btnStopEl = document.querySelector('button[data-stop]');
+let timerId = null;
 
- 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+btnStartEl.addEventListener('click', changeColorEverySecond);
+function changeColorEverySecond() {
+  timerId = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+    btnStartEl.disabled = true;
+  }, 1000);
+}
+
+btnStopEl.addEventListener('click', stopChangingColor);
+function stopChangingColor() {
+  btnStartEl.disabled = false;;
+  clearInterval(timerId);
+}
