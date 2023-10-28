2a7a5a3dcee17bbefa7b4e6067809fe6fbcfef95
imlaamer <147347232+imlaamer@users.noreply.github.com>
2023-10-28 19:23:29 +0000

    Deploying to gh-pages from @ imlaamer/goit-js-hw-09@b1856445a6a837782aaed203b6dadd7a977e3ad9 🚀

---
+++ 01-color-switcher.64ca56a0.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("body"),e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]");let o=null;e.addEventListener("click",(function(){o=setInterval((()=>{t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`,e.disabled=!0}),1e3)})),n.addEventListener("click",(function(){e.disabled=!1,clearInterval(o)}));
+//# sourceMappingURL=01-color-switcher.64ca56a0.js.map
